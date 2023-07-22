8f39decb4838e84438d26f6cfc20a196c2aa1d9c
Abdul7872 <aabdull787275@gmail.com>
2023-07-22 19:23:01 +0530

    Dev - fix cors problem on auth apis.

--- app/api/auth/sign-in/route.ts
+++ app/api/auth/sign-in/route.ts
@@ -3,25 +3,35 @@ import bcryptjs from 'bcryptjs';
 import prismadb from '@/lib/prismadb';
 import jwt from 'jsonwebtoken';
 
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
+export async function OPTIONS() {
+  return NextResponse.json({}, { headers: corsHeaders });
+}
+
 export async function POST( req: Request ) {
   try {
     const body = await req.json();
 
     const { email, password } = body;
     if (!email || !password) {
-      return new NextResponse("Please provide all fields!", { status: 400 });
+      return new NextResponse("Please provide all fields!", { status: 400, headers: corsHeaders });
     }
 
     // check user exist
     const user = await prismadb.user.findFirst({ where: {email} })
     if(!user) {
-      return new NextResponse("Invalid Credentials!", { status: 400 });
+      return new NextResponse("Invalid Credentials!", { status: 400, headers: corsHeaders });
     }
 
     // check password is correct
     const validPassword = await bcryptjs.compare(password, user.password);
     if(!validPassword) {
-      return new NextResponse("Invalid Credentials!", { status: 400 });
+      return new NextResponse("Invalid Credentials!", { status: 400, headers: corsHeaders });
     }
 
     //create token data
@@ -34,8 +44,9 @@ export async function POST( req: Request ) {
 
     const response = NextResponse.json({
       message: "Login successfully",
-      success: true
-    })
+      success: true,
+      token
+    },{headers: corsHeaders})
 
     // set token to cookies
     response.cookies?.set('token', token, {httpOnly: true})
@@ -44,6 +55,9 @@ export async function POST( req: Request ) {
 
   } catch (error) {
     console.log('[USER_POST]', error);
-    return new NextResponse("Internal error", { status: 500 });
+    return new NextResponse("Internal error", { 
+      status: 500, 
+      headers: corsHeaders
+    });
   }
 };
--- app/api/auth/sign-up/route.ts
+++ app/api/auth/sign-up/route.ts
@@ -2,24 +2,34 @@ import { NextResponse } from 'next/server';
 import bcryptjs from 'bcryptjs';
 import prismadb from '@/lib/prismadb';
 
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
+export async function OPTIONS() {
+  return NextResponse.json({}, { headers: corsHeaders });
+}
+
 export async function POST( req: Request ) {
   try {
     const body = await req.json();
 
     const { name, email, password } = body;
     if (!name || !email || !password) {
-      return new NextResponse("Please provide all fields!", { status: 400 });
+      return new NextResponse("Please provide all fields!", { status: 400, headers: corsHeaders });
     }
 
     const user = await prismadb.user.findFirst({ where: {email} })
     if(user) {
-      return new NextResponse("User already exists!", { status: 400 });
+      return new NextResponse("User already exists!", { status: 400, headers: corsHeaders });
     }
     // has password 
     const salt = await bcryptjs.genSalt(10);
     const hasPassword = await bcryptjs.hash(password, salt);
 
-    const newUser = await prismadb.user.create({
+    await prismadb.user.create({
       data: {
         name, 
         email, 
@@ -30,9 +40,9 @@ export async function POST( req: Request ) {
     return NextResponse.json({
       message: "User created successfully",
       success: true,
-    });
+    },{ headers: corsHeaders });
   } catch (error) {
     console.log('[USER_POST]', error);
-    return new NextResponse("Internal error", { status: 500 });
+    return new NextResponse("Internal error", { status: 500, headers: corsHeaders });
   }
 };
