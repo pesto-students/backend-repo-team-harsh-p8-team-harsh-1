beda60f41c9dbd070628cef0a495dc96976dd638
Abdul7872 <aabdull787275@gmail.com>
2023-07-22 23:25:15 +0530

    Fix - type issue of getDataFromToken fn

--- actions/get-data-from-token.ts
+++ actions/get-data-from-token.ts
@@ -2,13 +2,13 @@ import { cookies } from 'next/headers'
 import jwt from 'jsonwebtoken';
 
 interface JwtPayload {
-  userId: string,
-  userName: string,
-  userEmail: string,
-  isAdmin: boolean,
+  userId?: string,
+  userName?: string,
+  userEmail?: string,
+  isAdmin?: boolean,
 }
 
-export function getDataFromToken () {
+export function getDataFromToken ():JwtPayload {
   const cookieStore = cookies();
   const token = cookieStore?.get('token')?.value || '';
   const decodedToken = jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload;
